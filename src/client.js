41e9bac72a1176431a18c56576c82d5e02385232
DanielSimonsen90 <danielsimonsen90@gmail.com>
2025-02-04 00:25:33 +0100

    Update tab-size

--- src/client.js
+++ src/client.js
@@ -5,39 +5,39 @@ import Logger from './utils/logger.js';
 import crud from './crud.js';
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        IntentsBitField.Flags.GuildVoiceStates,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.Guilds,
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    IntentsBitField.Flags.GuildVoiceStates,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.Guilds,
+  ]
 });
 
 const discordConfig = ConfigurationManager.getDiscordConfig;
 const devMode = ConfigurationManager.getDevMode;
 
 client.once('ready', async () => {
-    Logger.info('Client is ready!');
-    await registerCommands(client, discordConfig);
-    if (devMode) {
-        client.user.setPresence({ activities: [{ name: 'in dev mode' }], status: 'online' });
-    } else {
-        client.user.setPresence({ activities: [{ name: 'Vinted', type: ActivityType.Watching }], status: 'online' });
-    }
+  Logger.info('Client is ready!');
+  await registerCommands(client, discordConfig);
+  if (devMode) {
+    client.user.setPresence({ activities: [{ name: 'in dev mode' }], status: 'online' });
+  } else {
+    client.user.setPresence({ activities: [{ name: 'Vinted', type: ActivityType.Watching }], status: 'online' });
+  }
 });
 
 // Change presence to show number of channels being monitored
 setInterval(async () => {
-    const channelCount = (crud.getAllVintedChannels()).length;
-    client.user.setPresence({ activities: [{ name: `${channelCount} channels`, type: ActivityType.Watching }], status: 'online' });
+  const channelCount = (crud.getAllVintedChannels()).length;
+  client.user.setPresence({ activities: [{ name: `${channelCount} channels`, type: ActivityType.Watching }], status: 'online' });
 }, 60000);
 
 client.on('interactionCreate', handleCommands);
 
 client.login(discordConfig.token).then((token) => {
-    Logger.info(`Logged in as ${client.user.tag}`);
+  Logger.info(`Logged in as ${client.user.tag}`);
 });
 
 export default client;
